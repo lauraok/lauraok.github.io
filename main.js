af730da0be393aff167802dadfa3bac15e4fef30
Laura Okas <lauraokas@Lauras-MacBook-Air.local>
2020-12-06 13:12:22 +0200

    mh

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ $('.tilt').tilt({
     glare: true,
     maxGlare: .5
 })
+$('.kast').tilt({
+    transform-style: preserve-3d
+    $('-kast1, kast2, kast3').tilt({
+      transform: translateZ(20px)
+    })
+})
